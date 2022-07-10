359dba6dcbaf2c5d72ab393257c6644dee046ab8
Kikai <Kikai@DESKTOP-KE8JND7>
2022-07-10 13:57:57 -0400

    Add project files.

---
+++ Prototype for Inventory Management/Form1.cs
@@ -0,0 +1,10 @@
+namespace Prototype_for_Inventory_Management
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
